08e8514990196094e0f29a39758765cc3604ef1e
milo <iitalics@gmail.com>
2025-01-15 23:25:17 -0500

    update main for testing

--- src/femlc/src/main.rs
+++ src/femlc/src/main.rs
@@ -1,42 +1,48 @@
 use feml::intern::Intern;
 use feml::parse::Parser;
+use feml::parse_tree::{Decl, ParseTree};
 use feml::token::Tokenizer;
 use std::process::ExitCode;
 
-static INPUT: &str = "
-def f (Q : A -> type) : (x : A) -> P == Q -> P x == Q x =
-  fn x => fn (p_eq_q : P == Q) =>
-    refl;
-";
-
-fn main() -> ExitCode {
-    let int = Intern::new();
-    let mut prs = Parser::new(&int);
-    let mut tkz = Tokenizer::new(INPUT);
+fn parse<'i>(intern: &'i Intern, input: &str) -> Result<ParseTree<'i>, String> {
+    let mut prs = Parser::new(intern);
+    let mut tkz = Tokenizer::new(input);
     for result in &mut tkz {
         let (loc, tk) = match result {
             Ok(x) => x,
-            Err(e) => {
-                eprint!("error: input:{}: {}", e.loc(), e);
-                return ExitCode::FAILURE;
-            }
+            Err(e) => return Err(format!("input:{}: {}", e.loc(), e)),
         };
         match prs.feed(loc, tk) {
             Ok(()) => {}
-            Err(e) => {
-                eprint!("error: input:{}: {}", e.loc(), e);
-                return ExitCode::FAILURE;
-            }
+            Err(e) => return Err(format!("input:{}: {}", e.loc(), e)),
         };
     }
-    let tree = match prs.end_of_file(tkz.loc()) {
-        Ok(x) => x,
-        Err(e) => {
-            eprint!("error: input:{}: {}", e.loc(), e);
+    match prs.end_of_file(tkz.loc()) {
+        Ok(x) => Ok(x),
+        Err(e) => Err(format!("input:{}: {}", e.loc(), e)),
+    }
+}
+
+fn main() -> ExitCode {
+    let int = Intern::new();
+    let tree = match parse(
+        &int,
+        "
+assert S (S (S Z)) : nat;
+",
+    ) {
+        Ok(t) => t,
+        Err(msg) => {
+            eprint!("error: {msg}");
             return ExitCode::FAILURE;
         }
     };
-    let decl = tree.decls()[0];
-    println!("{}", tree.display_decl(&int, decl));
+
+    for decl in tree.decls() {
+        if let Decl::Assert { exp, .. } = tree.view_decl(decl) {
+            println!("{}", tree.display_exp(&int, *exp));
+        }
+    }
+
     ExitCode::SUCCESS
 }
